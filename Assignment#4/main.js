12ee827faef7ae5f339b6195013e0f5742aff7f9
Abdul-Rehman Siddiqui <ar5488300@gmail.com>
2024-03-15 19:32:04 +0500

    first commit

---
+++ Assignment#4/main.js
@@ -0,0 +1,6 @@
+"use strict";
+//Assingnment # 4
+//storing famous quote 2 and its author
+let famous_person = "William Shakespeare";
+let message = "The fool doth think he is wise, but the wise man knows himself to be a fool.";
+console.log(`${famous_person} - "${message}"`);
---
+++ Assignment#4/main.ts
@@ -0,0 +1,11 @@
+//Assingnment # 4
+
+//storing famous quote 2 and its author
+
+let famous_person:string = "William Shakespeare"
+
+let message:string = "The fool doth think he is wise, but the wise man knows himself to be a fool."
+
+//printing the message
+
+console.log(`${famous_person} - "${message}"`)
